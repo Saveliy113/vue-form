0ccf0051febd3d07d2fe23751c6a130223cb4e07
Saveliy <sava2698@gmail.com>
2024-02-24 11:12:41 +0600

    Added footer component

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="footer">
+    <p>©2024 PayPal</p>
+    <div class="links">
+      <router-link to="/">Privacy</router-link>
+      <router-link to="/">Legal</router-link>
+      <router-link to="/">Contact</router-link>
+      <router-link to="/">Feedback</router-link>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.footer {
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  width: 45%;
+  margin: 0 auto;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+
+  > p {
+    margin: 0;
+    color: gray;
+  }
+
+  > .links {
+    display: flex;
+    gap: 8px;
+
+    > a {
+      color: unset;
+      font-weight: 500;
+    }
+  }
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,9 +2,11 @@
   <div>
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
 import Header from "src/components/layout/Header.vue";
+import Footer from "src/components/layout/Footer.vue";
 </script>
